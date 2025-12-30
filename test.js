9c2804ac6083bcc868e5b32e9b32de3ae62b894c
MaxyChay <sportznoah@gmail.com>
2025-12-30 21:46:47 +0700

    Update and rename test.js to variable.js

--- test.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World")
-let age = 5;
-console.log(`My Age is ${age}`)
---
+++ variable.js
@@ -0,0 +1,9 @@
+//Variable
+let age = 5;
+let fullname = "Maxwell";
+let online = true;
+let result = 4 + 5 * 5 + (37 + 1);
+console.log(`my age is ${age}`)
+console.log(`max is online: ${online}`)
+
+document.getElementById("MyH1").textContent =  result;
