baa92236722e0c49350ef06016866e9558c7498f
MaxyChay <sportznoah@gmail.com>
2025-12-30 21:48:44 +0700

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,6 @@
+// Prompt Input
+let username = window.prompt("Whats Your Name?");
+document.getElementById("MyH1").textContent = `Hi ${username}`;
+
+let ask = window.prompt("Kamu Umur Berapa?");
+document.getElementById("MyH2").textContent = `Kamu Umur = ${ask}`;
