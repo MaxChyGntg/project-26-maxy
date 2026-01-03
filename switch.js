6913b1f3d3ddc7b4a0d0678b8246044db32dca53
MaxChy <sportznoah@gmail.com>
2026-01-03 22:17:56 +0700

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,12 @@
+let day = 1;
+
+switch(day){
+    case 1: console.log("Ini Hari senin"); break;
+    case 2: console.log("Ini Hari Selasa"); break;
+    case 3: console.log("Ini Hari Rabu"); break;
+    case 4: console.log("Ini Hari Kamis"); break;
+    case 5: console.log("Ini Hari Jumat"); break;
+    case 6: console.log("Ini Hari Sabtu"); break;
+    case 7: console.log("Ini Hari Minggu"); break;
+    default: console.log(`${day} bukan hari gblk`);
+}
