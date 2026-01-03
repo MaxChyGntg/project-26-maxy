d7ec654845bea90ef1474944ae88c649709187c5
MaxChy <sportznoah@gmail.com>
2026-01-03 22:19:19 +0700

    Create stringmethod.js

---
+++ stringmethod.js
@@ -0,0 +1,16 @@
+let username = "sigma123";
+let result;
+
+result = username.startsWith(" ");
+if (result){
+    console.log("tdk valid");
+}
+else if(result = username.endsWith(" ")){
+    console.log("tdk valid");
+}
+else if(result = username.endsWith(".")){
+    console.log("tdk valid");
+}
+else{
+    console.log(username);
+}
