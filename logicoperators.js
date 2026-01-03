08b01e46ebdad7fb61a3f25883f0122530cc390f
MaxChy <sportznoah@gmail.com>
2026-01-03 22:21:48 +0700

    Create logicoperators.js

---
+++ logicoperators.js
@@ -0,0 +1,12 @@
+const weather = 20;
+
+if(weather < 0 || weather > 30){
+    console.log("Cuacanya Jelek");
+}
+else{
+    console.log("Cuaca bagus");
+}
+
+// && for AND
+// || for OR
+// ! for NOT
